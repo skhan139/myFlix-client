34e6d291750ee86092684c6917a65844c83e0df1
skhan139 <146803728+skhan139@users.noreply.github.com>
2024-03-16 20:03:45 -0400

    3.3

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,30 @@
+export const MovieView = ({ movie, onBackClick }) => {
+    return (
+      <div>
+        <div>
+          <img src={movie.image} style={{ width: '175px', height: 'auto' }} alt="Movie Poster" />
+        </div>
+        <div>
+          <span style={{fontWeight: 'bold' }}>Title: </span>
+          <span style={{fontWeight: 'bold' }}>{movie.title}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+        </div>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre}</span>
+        </div>
+        <div>
+          <span>Director: </span>
+          <span>{movie.director}</span>
+        </div>
+        <div>
+          <span>Featured: </span>
+          <span>{movie.featured}</span>
+        </div>
+        <button onClick={onBackClick}>Back</button>{" "}
+      </div>
+    );
+  };
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,12 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+ import { MainView } from "./components/main-view/main-view";
+ import "./index.scss";
 
-// Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
+ const App = () => {
+   return <MainView />;
+ };
 
-// Main component (will eventually use all the others)
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
-  );
-};
+ const container = document.querySelector("#root");
+ const root = createRoot(container);
 
-// Finds the root of your app
-const container = document.querySelector("#root");
-const root = createRoot(container);
-
-// Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+ root.render(<App />);
