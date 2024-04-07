2b36d298f2b4ac40dc30f0278f2064b5d8826135
skhan139 <146803728+skhan139@users.noreply.github.com>
2024-04-06 23:14:30 -0400

    3.5

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
  import { MovieCard } from "../movie-card/movie-card";
  import { MovieView } from "../movie-view/movie-view";
+ import { LoginView } from "../login-view/login-view";
+ import { SignupView } from "../signup-view/signup-view"
 
  export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+   const storedToken = localStorage.getItem("token");
+   const [user, setUser] = useState(storedUser? storedUser : null);
+   const [token, setToken] = useState(storedToken? storedToken : null);
     const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
    useEffect(() => {
-     fetch("https://movieapicf-30767e813dee.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://movieapicf-30767e813dee.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
        .then((response) => response.json())
        .then((data) => {
          const moviesFromApi = data.map((movie) => {
@@ -27,8 +39,22 @@ import { useState } from "react";
 
          setMovies(moviesFromApi);
        });
-   }, []);
+      }, [token]);
 
+      if (!user) {
+        return (
+          <>
+            <LoginView
+              onLoggedIn={(user, token) => {
+                setUser(user);
+                setToken(token);
+              }}
+            />
+            or
+            <SignupView />
+          </>
+        );
+      }
    if (selectedMovie) {
      return (
        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -39,6 +65,12 @@ import { useState } from "react";
   }
   return (
     <div>
+      <button onClick={() => { 
+         setUser(null); 
+         setToken(null); 
+         localStorage.clear();
+         }}>Logout</button>
+         
       {movies.map((movie) => (
         <MovieCard
           key={movie._id}
